e655a8af3ec2b67c35619584990882e35d7f9e1f
Yoon Gwan <blackgar@naver.com>
2022-03-08 17:36:44 +0900

    ETC : "upload NextJS Study"

---
+++ 프로젝트기획 및 사전학습/사전학습/윤관/NextJS 사전학습/캐럿마켓클론코딩/carrot-market/libs/client/useMutation.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+interface UseMutationState<T> {
+  loading: boolean;
+  data?: T;
+  error?: object;
+}
+
+type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>];
+
+export default function useMutation<T = any>(
+  url: string
+): UseMutationResult<T> {
+  const [state, setState] = useState<UseMutationState<T>>({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
+  // 위와 같이 정리해서 사용 가능. 나중에 한번 코드바꿔서 작성해보기
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<undefined | any>(undefined);
+  const [error, setError] = useState<undefined | any>(undefined);
+  function mutation(data: any) {
+    setLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json().catch(() => {}))
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }
+  return [mutation, { loading, data, error }];
+}
