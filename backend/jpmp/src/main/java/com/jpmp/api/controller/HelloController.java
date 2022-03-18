01be3190627d642f8efbcb209b686ec0888007b6
JangHyunJin <winckey3@gmail.com>
2022-03-18 10:13:36 +0900

    [BE] feat : swagger init 설정

--- backend/jpmp/src/main/java/com/jpmp/api/controller/HelloController.java
+++ backend/jpmp/src/main/java/com/jpmp/api/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.jpmp.api.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/member/v1")
+@Api(tags = {"회원가입 API"})
+@RequiredArgsConstructor
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    @PostMapping(value = "/")
+    public String signUp() {
+        return "signUp" ;
     }
+
 }
