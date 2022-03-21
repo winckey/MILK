10d74340ee8af57bf40acaa5c93913f87f5bb415
JangHyunJin <winckey3@gmail.com>
2022-03-22 02:14:32 +0900

    [BE] Feat : 스프링시큐리티 및 jwt 기능 추가

--- backend/jpmp/src/main/java/com/jpmp/db/entity/user/User.java
+++ backend/jpmp/src/main/java/com/jpmp/db/entity/user/User.java
@@ -2,12 +2,14 @@ package com.jpmp.db.entity.user;
 
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
 
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,4 +51,7 @@ public class User  {
     private UserImg profileImg;
 
 
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 }
