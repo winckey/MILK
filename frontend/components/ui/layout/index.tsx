dce1ddc3f73191112edc45941805e2786715668c
wonjong <bjlaancgk@naver.com>
2022-03-21 21:29:43 +0900

    [FE] 프로젝트 세팅

--- frontend/components/ui/layout/index.tsx
+++ frontend/components/ui/layout/index.tsx
@@ -1,3 +1,28 @@
-export default function Layout() {
-  return null;
+import Head from "next/head";
+import { Navbar } from "../common";
+
+interface LayoutProps {
+  title?: string;
+  canGoBack?: boolean;
+  hasTabBar?: boolean;
+  children: React.ReactNode;
+  seoTitle?: string;
+}
+
+export default function Layout({
+  title,
+  canGoBack,
+  hasTabBar,
+  children,
+  seoTitle,
+}: LayoutProps) {
+  return (
+    <div>
+      <Head>
+        <title>{seoTitle} | Carrot Market</title>
+      </Head>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
