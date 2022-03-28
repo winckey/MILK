558609e7ffaa4627a6817c7a9c55d3394f4cda24
장원종 <bjlaancgk@naver.com>
2022-03-28 19:04:33 +0000

    Update index.tsx

--- frontend/pages/product/[id]/index.tsx
+++ frontend/pages/product/[id]/index.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 const Product: NextPage = () => {
-  const [isOwner, setIsOwner] = useState(false)); // 본인 상품인지 여부
+  const [isOwner, setIsOwner] = useState(false); // 본인 상품인지 여부
   const [selectedOrder, setSelectedOrder] = useState<null | object>(null);
   const [selectedRealization, setSelectedRealization] = useState<null | object>(
     null
