2569748362f4a026803f8ef68dc68116a3a60338
JangHyunJin <winckey3@gmail.com>
2022-03-29 16:21:35 +0900

    [BE] Etc : rboard 이름 오류 수정

--- backend/jpmp/src/main/java/com/jpmp/api/service/nft/RBoardServiceImpl.java
+++ backend/jpmp/src/main/java/com/jpmp/api/service/nft/RBoardServiceImpl.java
@@ -6,41 +6,20 @@ import com.jpmp.db.entity.board.RealizationBoard;
 import com.jpmp.db.entity.nft.NFT;
 import com.jpmp.db.entity.user.User;
 import com.jpmp.db.repository.nft.NFTRepository;
-import com.jpmp.db.repository.nft.RBoradRepository;
+import com.jpmp.db.repository.nft.RBoardRepository;
 import com.jpmp.db.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.security.core.Authentication;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class RBoradServiceImpl implements RBoradService {
+public class RBoardServiceImpl implements RBoardService {
 
     private final UserRepository userRepository;
-    private final RBoradRepository rBoradRepository;
+    private final RBoardRepository rBoradRepository;
     private final NFTRepository nftRepository;
-
-//    @Override
-//    public void addRBorad(Authentication authentication, NtfRequestReqDto ntfRequestReqDto) {
-//
-////        User userDetails = (User) authentication.getDetails();
-////        User enterprise = userRepository.findByUserName(ntfRequestReqDto.getEntrepriseName()).get();
-////
-////        User userDetails = userRepository.findById(5L).get();
-////
-////
-////        RealizationBoard realizationBoard = RealizationBoard.builder()
-////                .nftId(ntfRequestReqDto.getNftId())
-////                .nftName(ntfRequestReqDto.getNftName())
-////                .nftImgUrl(ntfRequestReqDto.getImgUrl())
-////                .consumer(userDetails)
-////                .enterprise(enterprise)
-////                .build();
-////
-////        rBoradRepository.save(realizationBoard);
-//    }
 
     @Override
     public void addRBorad(User user, NtfRequestReqDto ntfRequestReqDto) {
@@ -55,5 +34,20 @@ public class RBoradServiceImpl implements RBoradService {
                 .build();
 
         rBoradRepository.save(realizationBoard);
+    }
+
+    @Override
+    public void getRBoradList(User user) {
+
+//        User enterprise = userRepository.findByRealname(ntfRequestReqDto.getEntrepriseName()).get();
+//        NFT nft = nftRepository.findByNftId(ntfRequestReqDto.getNftId()).get();
+//
+//        RealizationBoard realizationBoard = RealizationBoard.builder()
+//                .nft(nft)
+//                .consumer(user)
+//                .enterprise(enterprise)
+//                .build();
+//
+//        rBoradRepository.save(realizationBoard);
     }
 }
