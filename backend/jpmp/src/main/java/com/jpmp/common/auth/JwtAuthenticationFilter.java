3aca8e6e949e99597d09589a00b34d594160995b
JangHyunJin <winckey3@gmail.com>
2022-04-09 10:11:31 +0000

    milk 정리

--- backend/jpmp/src/main/java/com/jpmp/common/auth/JwtAuthenticationFilter.java
+++ backend/jpmp/src/main/java/com/jpmp/common/auth/JwtAuthenticationFilter.java
@@ -50,36 +50,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             //왜냐하면
             //Spring Security는 Spring 이전에 Filter 처리가 되므로 DispatcherServlet 까지 가지 않기 때문이다.
             //Security에는 각종 Filter를 상속받고 Filter 처리를 직접 작성할 수 있는데
-            //
             //여기서 catch 로 Exception 처리를 해줘야한다.
-
             String accessToken = getToken(request);
             //getToken 메서드로 헤더에서 JWT를 'Bearer '를 제외하여 가져옵니다. 만약, JWT를 프론트에서 주지 않았을 경우 null로 그대로 반환합니다.
-
             if (accessToken != null) {
                 checkLogout(accessToken);
                 //해당 토큰이 null이 아닐 경우는 이 토큰이 로그아웃된 토큰인지 검증합니다.
                 String username = jwtTokenUtil.getUsername(accessToken);
                 System.out.println("jwt인증필터 63 String username = jwtTokenUtil.getUsername(accessToken) : " + username);
-
-
                 //JwtTokenUtil에 선언된 메서드로 토큰에서 username을 가져옵니다.
-
                 if (username != null) {
                     UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
                     System.out.println("jwt인증필터 70 customUserDetailService.loadUserByUsername(username); : " + userDetails.getUsername());
                     //username이 null이 아닌 경우는 앞에서 만든 CustomUserDetailService에서 UserDetails객체를 가져옵니다.
-
                     validateAccessToken(accessToken, userDetails);
                     //이 토큰에서 추출한 username과 userDetailService에서 가져온 username이 맞는지 검증하고, 토큰의 유효성 검사를 진행합니다.
-
                     processSecurity(request, userDetails);
                     //검증 과정에 예외가 발생하지 않았다면, 해당 유저의 정보를 SecurityContext에 넣어줍니다.
                 }
             }
             filterChain.doFilter(request, response);
-
-
         } catch (MalformedJwtException e) {/// 위조됨
             sendErrorResponse(response, ErrorCode.FORGERY_TOKEN);
         } catch (ExpiredJwtException e) {// 만료
